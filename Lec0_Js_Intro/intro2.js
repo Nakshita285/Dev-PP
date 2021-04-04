b7d7ebf8b90d4ab5d1b651543e63370576c5e1a9
Nakshita <nakshita.malhotra.61@gmail.com>
2021-04-04 15:35:40 +0530

    git intro

--- Lec0_Js_Intro/intro2.js
+++ Lec0_Js_Intro/intro2.js
@@ -1,1 +1,37 @@
-console.log("hello guys this is my second program so much exited to do this!");
+// console.log("hello guys this is my second program so much exited to do this!");
+
+
+// objects => key value pair 
+
+data ={
+    "name": "nakshita",
+    "age": "20",
+    "gender": "female",
+
+}
+
+// console.log(data);
+// let key = "name";
+// console.log(data["name"]);
+
+// for (let key in data){
+//     console.log(key)
+// }
+
+
+data_intro = {
+    "name": {
+        "nakshita": "malhotra",
+        "hello": "world",
+        "questions": "answers",
+        "why so ": "tell me?",
+    },
+    "age": "10",
+    "gender": "female",
+};
+
+// console.log(data_intro);
+
+for (let key in data_intro){
+    console.log(key);
+}
