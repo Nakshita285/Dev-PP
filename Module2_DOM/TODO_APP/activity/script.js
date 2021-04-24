4d9550099b69e9828b4935a2fcd207a386ab4377
Nakshita <nakshita.malhotra.61@gmail.com>
2021-04-24 20:57:37 +0530

    Todo App

---
+++ Module2_DOM/TODO_APP/activity/script.js
@@ -0,0 +1,54 @@
+let inputTODO = document.querySelector('.todo-input');
+let addTodoButton = document.querySelector('.add-todo');
+let todosItems = document.querySelector('.todos-items');
+
+// event addition
+function addTodo(){
+    let todo = inputTODO.value;
+    if(todo){
+        let listItems = document.createElement("li");
+        listItems.classList.add("todo-items");
+
+        let pTag = document.createElement("p");
+        pTag.classList.add("todo");
+        pTag.innerHTML = todo;
+
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-tasks");
+        deleteButton.innerHTML = "Delete Task";
+
+        deleteButton.addEventListener(  "click", function(event){
+            // console.log("delete button is clicked!!" );
+            // console.log(event);
+            event.target.parentNode.remove();
+            // event.target.parentNode.childNodes[0].remove();
+        })
+
+        listItems.append(pTag, deleteButton);
+
+        // appending li item to ul 
+        todosItems.append(listItems);
+
+        inputTODO.value = "";
+    }
+    else{
+        alert("Oops! Incorrect or no input");
+    }
+}
+
+addTodoButton.addEventListener( "click" , function(){
+    addTodo();
+})
+
+
+inputTODO.addEventListener( "keypress", function(event){
+    if(event.key == "Enter"){
+        addTodo();
+    }
+})
+    /* 
+        <li class="todo-items">
+            <p class="todo">Todo</p>
+            <button class="delete-tasks">Delete</button>
+        </li>
+        */
